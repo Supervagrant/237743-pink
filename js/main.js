afcd89122a68ad6fa9e402e4ed9f01cee4224e25
Supervagrant <supertramp@tut.by>
2017-06-15 02:16:35 +0300

    неразбериха с js и открытием/закрытием меню

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-
 var menuOpen = document.querySelector (".toggle-nav__btn");
 var menuPopup = document.querySelector(".toggle-nav__cross");
+var menuClose = document.querySelector(".main-nav__list--closed");
+var menuView = document.querySelector(".main-nav__list");
+
 
 
 
 menuOpen.addEventListener("click", function(event) {
   event.preventDefault();
   menuOpen.classList.toggle("toggle-nav__cross");
+  menuClose.classList.remove("main-nav__list--closed");
 });
-
 menuPopup.addEventListener("click", function(event) {
   event.preventDefault();
-  menuPopup.classList.toggle("toggle-nav__cross");
+  menuPopup.classList.toggle("toggle-nav__btn");
+  menuView.classList.remove("main-nav__list");
 });
+
